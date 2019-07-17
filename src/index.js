be0a88bbdda4b42c97cf35c536e8895d63ddaf81
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-17 00:27:43 -0300

    Imported routes from index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.use(express.json())
+app.use(require('./routes'))
+
+
  
 app.listen(3000)
