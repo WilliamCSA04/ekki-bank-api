8d9dab5e67ce43e82454e0d69003d4e3cc6895ea
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-17 00:14:44 -0300

    Changed account table name

--- src/migrations/20190717021714-create-account.js
+++ src/migrations/20190717021714-create-account.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Accounts', {
+    return queryInterface.createTable('accounts', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -35,6 +35,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Accounts');
+    return queryInterface.dropTable('accounts');
   }
 };
