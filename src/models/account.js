9a11a29f47b94a85bc4579d048a58f4799da7bfb
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-19 04:08:42 -0300

    Started logic to transfer money between accounts

--- src/models/account.js
+++ src/models/account.js
@@ -10,5 +10,26 @@ module.exports = (sequelize, DataTypes) => {
       onDelete: 'CASCADE'
     });
   };
+
+  Account.prototype.transfer = function(targetUserId, amount){
+    const newBalance = this.balance - value;
+    const doesNotHaveEnoughBalance = newBalance < 0;
+    if(doesNotHaveEnoughBalance){
+      const extractFromLimit = Math.abs(newBalance);
+      const newLimit = this.limit - extractFromLimit;
+      const doesNotHaveEnoughLimit = this.limit < 0;
+      if(doesNotHaveEnoughLimit){
+        //TODO: Return when account can't execute a transfer
+      }
+      this.executeTranference(0, newLimit, amount)
+    }else{
+      this.executeTranference(newBalance, this.limit, amount)
+    }
+  }
+
+  Account.prototype.executeTranference = function(newBalance, newLimit, amount){
+    //TODO: Logic to move money to new account
+  }
+
   return Account;
 };
