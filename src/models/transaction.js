6dd8e74086434f170e678d475f53dd83da2349d8
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-19 03:49:37 -0300

    Created logic to register a new transaction

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -13,5 +13,40 @@ module.exports = (sequelize, DataTypes) => {
       as: 'toUser',
     })
   };
+
+  Transaction.register = function(sourceUserId, targetUserId){
+    const lastTransactionsBetweenUsers = Transaction.getLastTransactionBetweenUsers(sourceUserId, targetUserId)
+    const minutesBetweenTransactions = lastTransactionsBetweenUsers.timeDifference();
+    const lessThanTwoMinutes = minutesBetweenTransactions < 2
+    if(lessThanTwoMinutes){
+      return lastTransactionsBetweenUsers.replaceTransaction();
+    }else{
+      return Transaction.create({ value, sourceUserId, targetUserId });
+    }
+  }
+
+  Transaction.prototype.replaceTransaction = function(){
+    const { value, fromUserId, toUserId } = lastTransactionsBetweenUsers;
+    lastTransactionsBetweenUsers.destroy();
+    return Transaction.create({ value, fromUserId, toUserId });
+  }
+
+  Transaction.getLastTransactionBetweenUsers = function(sourceUserId, targetUserId){
+    const queryObject = { 
+      where: {fromUserId: sourceUserId, toUserId: targetUserId}, 
+      order: [['createdAt', 'DESC']]
+    }
+    return Transaction.findAll(queryObject)[0]
+  }
+
+  Transaction.prototype.timeDifference = function(){
+    const present = Date.now().getTime();
+    const creationDate = this.createdAt.getTime();
+    const diff = ((present - creationDate)/1000)/60;
+    const roundedDiff = Math.floor(diff);
+    const diffInMinutes = Math.abs(roundedDiff);
+    return diffInMinutes;
+  }
+
   return Transaction;
 };
