ddd2a79a18f49832eeb3a4ebcc11fc8ccb62a4a4
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-18 23:44:12 -0300

    Added route to create user

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const controllers = require('./controllers')
+const { users } = controllers
 
 const routes = express.Router();
 
-routes.get('/', function (req, res) {
-  res.send('Ekki bank api')
-})
+//Users
+routes.post('/user', users.create)
 
 module.exports = routes;
