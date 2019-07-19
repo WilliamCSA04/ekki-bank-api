b020c525cb2955eec6e401c336bcc14502adad73
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-19 01:42:42 -0300

    Added method to create a contact

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,1 +1,13 @@
 import { Contact } from '../models'
+
+function create(req, res){
+  const { body } = req;
+  const contact = Contact.create(body).then(contact => {
+    const responseJson = { contact, message: 'Contato salvo com sucesso' }
+    res.status(200).json(responseJson);
+  }).catch(err => {
+    const responseJson = { message: 'Não foi possivel salvar seu contato' }
+    res.status(400).json(responseJson);
+  });
+  return contact;
+}
