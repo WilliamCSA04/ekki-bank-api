5f51b187c476220eac8404847e5f6526f6638693
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-20 02:10:46 -0300

    Fix: Transfer controller function

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -2,13 +2,20 @@ import { Account } from '../models'
 import { Module } from 'module';
 
 function transfer(req, res){
-  const transfer = Account.transfer(req.body).then(account => {
-    const responseJson = { account, message: 'Transferencia executada com sucesso' }
-    res.status(200).json(responseJson);
+  const { accountId, targetUserId, amount } = req.body;
+  const transfer = Account.findOne({where: {id: accountId}}).then(account => {
+    Account.transfer(targetUserId, amount).then(account => {
+      const responseJson = { account, message: 'Transferencia executada com sucesso' }
+      res.status(200).json(responseJson);
+    }).catch(error => {
+      const responseJson = { message: 'Houve um erro enquanto tentavamos processar sua transação' }
+      res.status(400).json(responseJson)
+    })
   }).catch(error => {
-    const responseJson = { message: 'Houve um erro enquanto tentavamos processar sua transação' }
+    const responseJson = { message: 'Não foi possivel encontrar sua conta' }
     res.status(400).json(responseJson)
   })
+  
   return transfer;
 }
 
