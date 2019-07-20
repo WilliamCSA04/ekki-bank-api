818098370adebd0d0b2bdd9b75944b9c1784f2de
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-20 01:55:44 -0300

    Added transfer function to accounts controller

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -1,1 +1,12 @@
 import { Account } from '../models'
+
+function transfer(req, res){
+  const transfer = Account.transfer(req.body).then(account => {
+    const responseJson = { account, message: 'Transferencia executada com sucesso' }
+    res.status(200).json(responseJson);
+  }).catch(error => {
+    const responseJson = { message: 'Houve um erro enquanto tentavamos processar sua transação' }
+    res.status(400).json(responseJson)
+  })
+  return transfer;
+}
