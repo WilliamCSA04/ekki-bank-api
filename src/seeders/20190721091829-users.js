458be3e841a56861c7cf1ea21fae9c7295dc65de
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-21 06:52:33 -0300

    Fix: Users seeds

--- src/seeders/20190721091829-users.js
+++ src/seeders/20190721091829-users.js
@@ -1,19 +1,23 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Users', 
+  up: (queryInterface, Sequelize) => queryInterface.bulkInsert('users', 
   [
     {
       name: 'Irelia',
       cpf: '12345678998',
       phone: '(51)912345678',
+      createdAt: new Date(),
+      updatedAt: new Date()
     },
     {
       name: 'Fiora',
       cpf: '67899812345',
       phone: '(51)956781234',
+      createdAt: new Date(),
+      updatedAt: new Date()
     }
   ], {}),
 
-  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Users', null, {})
+  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('users', null, {})
 };
