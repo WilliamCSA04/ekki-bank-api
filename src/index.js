83131be92aa74d6cb5776e9eb45bd1ceaec44c11
Williamcsa04 <williamcaetano14@gmail.com>
2019-07-22 10:35:31 -0300

    Added socket.io

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 
+const io = require('socket.io')(server);
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
 app.use(cors());
 app.use(express.json())
 app.use(require('./routes'))
